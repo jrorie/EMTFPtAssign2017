47145d73a2f85759420b6e1009b64d38938b18ab
Jamal <jrorie@rice.edu>
2017-05-02 21:05:02 +0200

    Updated configs to include JTR output directories

--- configs/PtRegression_Apr_2017/User.h
+++ configs/PtRegression_Apr_2017/User.h
@@ -5,17 +5,18 @@ TString OUT_FILE_NAME = "PtRegression_Apr_2017";  // Name base for output ROOT f
 TString EOS_DIR_NAME  = "root://eoscms.cern.ch//store/user/abrinke1/EMTF/Emulator/ntuples";  // Input directory in eos
 
 namespace PtRegression_Apr_2017_cfg {
-  
+
   inline void ConfigureUser( const TString USER ) {
     
     std::cout << "\nConfiguring PtRegression_Apr_2017 code for user " << USER << std::endl;
     
     if (USER == "AWB") {
-      EOS_DIR_NAME = "root://eoscms.cern.ch//store/user/abrinke1/EMTF/Emulator/ntuples";  // Input directory in eos
-      OUT_DIR_NAME = "/afs/cern.ch/work/a/abrinke1/public/EMTF/PtAssign2017";
-      OUT_FILE_NAME = "PtRegression_Apr_2017_04_28";
+      OUT_DIR_NAME  = "/afs/cern.ch/work/a/abrinke1/public/EMTF/PtAssign2017";
+      OUT_FILE_NAME = "PtRegression_Apr_2017_04_20";
     }
-    if (USER == "JTR") {
+    if (USER == "JTR_lxplus") {
+      OUT_DIR_NAME  = "/afs/cern.ch/work/j/jrorie/public/EMTF_pT_Training";
+      OUT_FILE_NAME = "PtRegression_May_2017_04_20";
     }
     
   } // End function: inline void ConfigureUser()    
--- configs/RateVsEff/General.h
+++ configs/RateVsEff/General.h
@@ -1,18 +1,18 @@
 
 // *** User *** ///
-const TString USER = "AWB";  // Settings applied in User.h
+const TString USER = "JTR_lxplus";  // Settings applied in User.h
 
 // *** Events to process *** //
 const int MAX_EVT    =      -1;  // Number of events to process
 const int REPORT_EVT =  100000;  // Report every Nth event during processing
 
 // *** Muon / track settings *** //
-const double ETAMIN  = 1.20;  // Minimum GEN / trigger eta to consider
+const double ETAMIN  = 1.24;  // Minimum GEN / trigger eta to consider
 const double ETAMAX  = 2.40;  // Maximum GEN / trigger eta to consider
 
 // *** Rate plot settings *** //
-const double PTMIN   =  0.0;  // Minimum pT
-const double PTMAX   = 50.0;  // Maximum pT
+const double PTMIN   =  1.0;  // Minimum pT
+const double PTMAX   = 51.0;  // Maximum pT
 const int    PTBINS  =   50;  // Number of bins in rate plot
 
 const int    PTDIVS  =   10;  // Number of trigger pT bins per GEN pT bin, before scaling
